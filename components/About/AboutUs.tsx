b151ccb28aa3f25400a8001dbd32e2985d8d4124
Souradip2022 <22052939@kiit.ac.in>
2024-06-06 22:32:24 +0530

    Optimised AboutUs of about us section for mobile view

--- components/About/AboutUs.tsx
+++ components/About/AboutUs.tsx
@@ -1,15 +1,17 @@
-import { Sparkle } from "@/constants/Icons";
+import {Sparkle} from "@/constants/Icons";
 import React from "react";
 
 function AboutUs() {
   return (
-    <div className="h-screen bg-black text-white py-24">
-      <div className="flex items-center gap-2 ml-24 text-2xl">
-        <Sparkle /> <span className="uppercase">About US</span>
+    <div className="h-fit md:h-screen bg-black text-white py-9 md:py-24">
+      <div className="flex w-full items-center justify-center text-2xl py-9 relative">
+        <div className="w-fit flex items-center justify-center md:absolute md:left-24">
+          <Sparkle/> <span className="uppercase">About US</span>
+        </div>
       </div>
-      <div className="h-full grid place-items-center">
-        <div className="px-56">
-          <span className="text-3xl font-semibold px-56 inline-block">
+      <div className="h-full grid place-items-center gap-y-24">
+        <div className="md:px-56">
+          <span className="text-xl md:text-3xl font-semibold px-5 text-center md:px-56 inline-block w-full">
             We are a{" "}
             <span className="text-[#7B61FF]">
               team of storytellers, creative wizards and tech geeks
@@ -18,24 +20,24 @@ function AboutUs() {
             <span className="text-[#7B61FF]">your story better</span>
           </span>
         </div>
-        <div className="flex items-center justify-around gap-20">
+        <div className="flex items-center justify-around gap-x-7 md:gap-20">
           <div className=" border-b-4 border-[#7B61FF] text-center">
-            <span className="text-[#7B61FF] text-7xl">45+</span>
-            <br />
+            <span className="text-[#7B61FF] text-6xl md:text-7xl">45+</span>
+            <br/>
             <span>Brands</span>
           </div>
           <div className=" border-b-4 border-[#7B61FF] text-center">
-            <span className="text-[#7B61FF] text-7xl">20+</span>
-            <br />
+            <span className="text-[#7B61FF] text-6xl md:text-7xl">20+</span>
+            <br/>
             <span>Domains</span>
           </div>
           <div className=" border-b-4 border-[#7B61FF] text-center">
-            <span className="text-[#7B61FF] text-7xl">4+</span>
-            <br />
+            <span className="text-[#7B61FF] text-6xl md:text-7xl">4+</span>
+            <br/>
             <span>Countries</span>
           </div>
         </div>
-        <div className="mx-72 px-60">
+        <div className="md:mx-72 md:px-60 px-5 text-center text-xs md:text-lg">
           We are a Brand Strategy and Content Marketing agency, helping
           companies to brand better with tailored strategies and convey their
           business stories better than ever.
