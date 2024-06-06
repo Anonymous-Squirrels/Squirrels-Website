1dabe6eb8c1659b154b11e66e344bf8d9e133082
Souradip2022 <22052939@kiit.ac.in>
2024-06-06 11:36:51 +0530

    Optimised UIDesign of services for mobile device

--- components/Services/UIDesign.tsx
+++ components/Services/UIDesign.tsx
@@ -1,30 +1,40 @@
 import React from "react";
 import FAB from "../ui/FAB";
-import { Logo2 } from "@/constants/Icons";
+import {Logo2} from "@/constants/Icons";
 import ServiceAccordian from "./ServiceAccordian";
 import Image from "next/image";
 
 function UIDesign() {
   return (
-    <div className="bg-white grid place-items-center py-5 min-h-screen gap-10">
+    <div className="bg-white grid place-items-center pt-5 md:py-5 min-h-screen gap-10">
       <FAB variant="secondary">UI Design</FAB>
-      <div className="text-[#5C3095] text-4xl font-bold w-96 text-center">
+      <div className="text-[#5C3095] text-3xl md:text-4xl font-bold w-[85vw] md:w-96 text-center">
         Get The Best Design, No Compromise
       </div>
-      <div className="flex items-center px-56 gap-32">
-        <ServiceAccordian />
+      <div className="flex w-screen md:w-fit flex-col md:flex-row items-center  gap-0 md:gap-32">
+        <div className="w-full px-7 md:px-0 md:w-[720px]">
+          <ServiceAccordian/>
+        </div>
         <Image
-          src="/assets/billboard.png"
-          alt="billboard"
+          src="/assets/gradient.png"
+          alt="gradient"
           width={500}
           height={500}
+          className="hidden md:flex"
+        />
+        <Image
+          src="/assets/gradient.png"
+          alt="gradient"
+          width={430}
+          height={500}
+          className="flex md:hidden w-full h-[140px]"
         />
       </div>
-      <div className="flex items-center gap-8 px-48">
-        <Logo2 />
+      <div className="hidden md:flex items-center gap-8 px-48">
+        <Logo2/>
         <div className="text-[#5C3095] text-2xl font-semibold">
           Making design is a no-brainer for our professionals,
-          <br /> So you can focus on what matters and feel free to inquire!
+          <br/> So you can focus on what matters and feel free to inquire!
         </div>
       </div>
     </div>
