3614882d2aaa428effe09afc70cf1a087f39ad83
Souradip2022 <22052939@kiit.ac.in>
2024-06-06 13:11:25 +0530

    Optimised Engage component of services for mobile view

--- components/Services/Engage.tsx
+++ components/Services/Engage.tsx
@@ -3,12 +3,12 @@ import Button from "../ui/Button";
 
 function Engage() {
   return (
-    <div className="bg-[#8F00FF] px-96 py-16 text-white">
-      <div className="text-4xl font-bold text-center my-5">
+    <div className="bg-[#8F00FF] px-7 md:px-96 py-1 md:py-16 text-white">
+      <div className="text-3xl md:text-4xl font-bold text-center my-5">
         Engage Your Audience <br />
         Grow Your Ideas.
       </div>
-      <div className="my-5 px-32 text-sm font-thin text-center">
+      <div className="my-5 md:px-32 text-sm text-center">
         Connect with Anonymous Squirrels today to boost your creativity and
         promote your brand with maximum potential.
       </div>
