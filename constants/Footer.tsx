7dcac0793940246f949fc27f5079b2f52151a9e9
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2024-06-28 02:27:22 +0530

    Footer Links Added

---
+++ constants/Footer.tsx
@@ -0,0 +1,30 @@
+export const developers = [
+  {
+    id: 1,
+    name: "Soham",
+    designation: "UI/UX Designer",
+    image: "/profile/soham.webp",
+    linkedIn: "https://www.linkedin.com/in/sohan-pattnaik-7a318817b",
+  },
+  {
+    id: 2,
+    name: "Ashish",
+    designation: "UI/UX Designer",
+    image: "/profile/ashish.webp",
+    linkedIn: "https://www.linkedin.com/in/ashish-pothal/",
+  },
+  {
+    id: 4,
+    name: "Souradip",
+    designation: "Web Developer",
+    image: "",
+    linkedIn: "https://www.google.com",
+  },
+  {
+    id: 3,
+    name: "Priyanshu",
+    designation: "Web Developer",
+    image: "/profile/priyanshu.webp",
+    linkedIn: "https://www.linkedin.com/in/kpriyanshu2003/",
+  },
+];
