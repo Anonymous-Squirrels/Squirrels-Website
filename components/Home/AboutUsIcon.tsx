bd4a62a7dc8cb5a22877e63910ff8fe0af3ab9a1
Kumar Priyanshu <az.priyanshu.rockstar@gmail.com>
2024-06-28 02:33:37 +0530

    Improved Engage + About Us Animation

--- components/Home/AboutUsIcon.tsx
+++ components/Home/AboutUsIcon.tsx
@@ -1,6 +1,6 @@
-"use client"
-import React, {useEffect, useState} from 'react';
-import {LogoLarge} from "@/constants/Icons";
+"use client";
+import React, { useState } from "react";
+import { LogoLarge } from "@/constants/Icons";
 
 function AboutUsIcon() {
   const [color, setColor] = useState<boolean>(false);
@@ -8,14 +8,14 @@ function AboutUsIcon() {
   return (
     <p
       className="z-20"
-      onMouseEnter={() => setTimeout(() => setColor(true), 300)
-      }
-      onMouseLeave={() => setTimeout(() => setColor(false), 200)}
-      onClick={() => {
-        setColor(true);
-        setTimeout(() => setColor(false), 500);
-      }}>
-      <LogoLarge className="w-[110px] h-[80px] md:w-[190px] md:h-[155px]" color={color ? "#7B61FF" : "#E7E7E7"}/>
+      onMouseEnter={() => setColor(true)}
+      onMouseLeave={() => setColor(false)}
+      onClick={() => setColor(!color)}
+    >
+      <LogoLarge
+        className="w-[110px] h-[80px] md:w-[190px] md:h-[155px] duration-200 transition-color"
+        color={color ? "#7B61FF" : "#E7E7E7"}
+      />
     </p>
   );
 }
