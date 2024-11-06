d288128b67bbab06b809670149831ee2ab245e50
Soumyadip Maity <22053029@kiit.ac.in>
2024-11-06 18:29:13 +0530

    feat: add LightContactBackground component for dark mode support

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,38 @@
-import AboutUs from "@/components/Home/AboutUs";
-import {BentoBox} from "@/components/Home/BentoBox";
-import Home from "@/components/Home/HeroSection";
-import Testimonials from "@/components/Home/Testimonials";
-import React from "react";
-import {Blob} from "@/constants/Icons";
-import Engage from "@/components/Home/Engage";
-import ContactBackground from "@/components/Contact/ContactBackground";
+import AboutUs from '@/components/Home/AboutUs';
+import { BentoBox } from '@/components/Home/BentoBox';
+import Home from '@/components/Home/HeroSection';
+import Testimonials from '@/components/Home/Testimonials';
+import React from 'react';
+import { Blob } from '@/constants/Icons';
+import Engage from '@/components/Home/Engage';
+import ContactBackground from '@/components/Contact/ContactBackground';
+import LightContactBackground from '@/components/Contact/LightContactBackground';
 
 function Page() {
   return (
     <div className="scrollbar-hide cursor-default">
       <div className="w-full relative overflow-hidden">
         <div className="absolute md:hidden -translate-x-1/2 -top-52 z-10">
-          <Blob variant="violet"/>
+          <Blob variant="violet" />
         </div>
         <div className="absolute -right-10 translate-x-1/2 -top-80 z-10 md:hidden">
-          <Blob variant="yellow"/>
+          <Blob variant="yellow" />
         </div>
-        <Home/>
+        <Home />
         <div className="hidden md:block w-full h-[700px]">
-          <Engage/>
+          <Engage />
         </div>
-        <AboutUs/>
+        <AboutUs />
       </div>
-      <BentoBox/>
-      <Testimonials/>
-      <ContactBackground/>
+      <BentoBox />
+      <Testimonials />
+      <div className='hidden dark:block'>
+      <ContactBackground />
+
+      </div>
+      <div className='dark:hidden block'>
+  <LightContactBackground/>
+</div>
     </div>
   );
 }
