37e05c1681995b84e31f403c95c1dc4d0a45e9aa
Soumyadip Maity <22053029@kiit.ac.in>
2024-11-06 18:30:21 +0530

    feat: enhance AboutUs component with dark mode support, updated content, and improved styling

--- components/Home/AboutUs.tsx
+++ components/Home/AboutUs.tsx
@@ -1,14 +1,14 @@
-import React from "react";
-import Button from "../ui/Button";
-import Image from "next/image";
-import {ArrowRight} from "@/constants/Icons";
-import Link from "next/link";
+import React from 'react';
+import Button from '../ui/Button';
+import Image from 'next/image';
+import { LightArrowRight, DarkArrowRight } from '@/constants/Icons';
+import Link from 'next/link';
 
 function AboutUs() {
   return (
-    <div id={"about-us"} className=" bg-black text-white">
+    <div id={'about-us'} className=" dark:bg-black bg-light-purple text-black dark:text-white">
       <div className="flex pt-7 px-7 p-7 md:p-14 md:pt-10">
-        <span className="uppercase text-5xl md:text-8xl font-bold mx-auto  bg-gradient-to-r from-[#A998FF] via-[#7B61FF] to-[#5237D8] inline-block text-transparent bg-clip-text">
+        <span className="uppercase text-5xl md:text-8xl font-bold mx-auto  bg-gradient-to-r dark:from-[#A998FF] from-[#5C3095] dark:via-[#7B61FF] via-[#6D5ACB] dark:to-[#5237D8] to-[#5237D8] inline-block text-transparent bg-clip-text">
           About Us
         </span>
       </div>
@@ -16,33 +16,41 @@ function AboutUs() {
       <div className="flex flex-col items-center justify-between">
         <div className="flex flex-col items-center justify-between gap-y-10">
           <div className="text-center text-xl md:text-2xl px-8 md:px-0 font-bold max-w-[900px] italic">
-            We are a team of{" "}
-            <span className="text-[#9C3DFC] italic">
-            storytellers, creative wizards and tech geeks
-          </span>{" "}
-            who help you convey your story better.
-            <br/>
-            <br/>
+            We are a team of{' '}
+            <span className="dark:text-[#9C3DFC] text-dark-purple italic">
+              storytellers, creative wizards and tech geeks
+            </span>{' '}
+            {/* who help you convey your story better. */}
+            who help you convert more patients & build a brand.
+            <br />
+            <br />
             <div className="text-base md:text-lg px-5 font-normal">
-              We are a Brand Strategy and Content Marketing company with over{" "}
+              {/* We are a Brand Strategy and Content Marketing company with over{" "}
               <span className="text-[#BE96FF]">50+ names in our clientele.</span>{" "}
               We specialize in brand communications and transforming your
               companies into brands that people will remember.
               Our team is here to{" "}
               <span className="text-[#BE96FF]">
               take your branding and game to the next level.
-            </span>
+            </span> */}
+              As a leading healthcare and dental marketing agency, we&apos;re
+              dedicated to crafting compelling brand stories that resonate with
+              your audience. Our team of creative experts and digital
+              strategists will help you elevate your brand and drive results.
             </div>
           </div>
 
           <div className="grid place-items-center ">
-            <Link href={"/about"}>
+            <Link href={'/about'}>
               <Button
                 variant="secondary"
-                className="text-white flex items-center gap-3 group"
+          
+                className="dark:text-white text-dark-purple hover:text-white flex items-center gap-3 group"
               >
                 Know More
-                <ArrowRight className="group-active:rotate-45 md:group-hover:rotate-45 transition-all duration-300"/>
+                <LightArrowRight className=" text-black dark:text-white hidden dark:block group-active:rotate-45 md:group-hover:rotate-45 transition-all duration-300" />
+                <DarkArrowRight className=" text-black dark:text-white block dark:hidden group-active:rotate-45 md:group-hover:rotate-45 transition-all duration-300" />
+
               </Button>
             </Link>
           </div>
@@ -54,10 +62,9 @@ function AboutUs() {
             alt="gradient"
             width={1500}
             height={350}
-            className="w-full h-28 md:h-[350px]"
+            className="w-full h-28 md:h-[225px] lg:h-[350px]"
           />
         </div>
-
       </div>
     </div>
   );
