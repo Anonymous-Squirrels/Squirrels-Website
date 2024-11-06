bae65a96a5e0ebcc98c024e84a61b88a908880db
Soumyadip Maity <22053029@kiit.ac.in>
2024-11-06 18:31:18 +0530

    feat: enhance ThreeStepProcess component with dark mode support and improved styling

--- components/About/ThreeStepProcess.tsx
+++ components/About/ThreeStepProcess.tsx
@@ -1,35 +1,39 @@
-import {Sparkle} from "@/constants/Icons";
-import React from "react";
-import ProcessCard from "./ProcessCard";
-import { OUR_PROCESS } from "@/constants/ProcessData";
-import Image from "next/image";
+import { Sparkle } from '@/constants/Icons';
+import React from 'react';
+import ProcessCard from './ProcessCard';
+import { OUR_PROCESS } from '@/constants/ProcessData';
+import Image from 'next/image';
 
 function ThreeStepProcess() {
-
   return (
-    <div className="h-fit md:h-fit bg-black text-white px-7 py-14 md:p-24 overflow-hidden">
+    <div className="h-fit md:h-fit dark:bg-black bg-[#dad9ff] dark:text-white text-black px-7 py-14 md:p-24 overflow-hidden">
       <div className="flex w-full text-2xl md:text-2xl gap-3.5 items-center justify-center md:justify-start z-20">
-        <Sparkle/>
+        <Sparkle />
         <span>OUR 3-STEP PROCESS</span>
       </div>
-      
 
       <div className="flex w-full 2xl:w-[100%] 3xl:w-[90%] flex-wrap gap-3 md:gap-1 2xl:gap-[30px] sm:pl-10 lg:pl-12 xl:w-[90%] 2xl:pl-[70px] pb-8 justify-center mx-auto">
-  {OUR_PROCESS.map((process, index) => {
-    return (
-      <div key={index} className="flex justify-center">
-        <ProcessCard
-          isHome={true}
-          key={index}
-          logo={<Image src={process.imgSrc} alt="support" width={500} height={500}></Image>}
-          heading={process.name}
-          about={process.about}
-        ></ProcessCard>
+        {OUR_PROCESS.map((process, index) => {
+          return (
+            <div key={index} className="flex justify-center">
+              <ProcessCard
+                isHome={true}
+                key={index}
+                logo={
+                  <Image
+                    src={process.imgSrc}
+                    alt="support"
+                    width={500}
+                    height={500}
+                  ></Image>
+                }
+                heading={process.name}
+                about={process.about}
+              ></ProcessCard>
+            </div>
+          );
+        })}
       </div>
-    );
-  })}
-</div>
-
     </div>
   );
 }
