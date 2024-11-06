d1b6f6397972757a52ba6f17d06c56813ae86dab
Soumyadip Maity <22053029@kiit.ac.in>
2024-11-06 18:30:04 +0530

    feat: enhance BentoBox component with light and dark theme support, improved layout, and styling

--- components/Home/BentoBox.tsx
+++ components/Home/BentoBox.tsx
@@ -1,60 +1,117 @@
-"use client";
-import React from "react";
-import { BentoItems } from "@/constants/BentoBox";
-import GlassMorphism from "@/components/ui/GlassMorphism";
+'use client';
+import React from 'react';
+import { BentoItems } from '@/constants/BentoBox';
+import GlassMorphism from '@/components/ui/GlassMorphism';
 
 export function BentoBox() {
   return (
-    <div className="bg-[url('/assets/baltic-rose.webp')] bg-cover text-white p-10 md:p-32">
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10">
-        {/* Render all four boxes */}
-        {BentoItems.slice(0, 2).map((item, i) => (
-          <GlassMorphism
-            key={i}
-            variant={"light"}
-            className=" md:min-h-[400px] h-full overflow-hidden rounded-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:-white/[0.2] space-y-4 w-full place-items-center"
-          >
-            <div className="text-sm transition duration-200 w-full flex flex-col items-center">
-              <p className="text-center text-xl font-bold block mb-5">
-                {item.title}
-              </p>
 
-              <p className="text-center text-xs md:text-lg inline-block text-[#D2D2D2]">
-                {item.description}
-              </p>
-            </div>
+    //light theme 
+    <div>
+      <div className="bg-[url('/assets/LightbgBentobox.png')] block dark:hidden bg-cover text-black p-10 lg:p-32">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10">
+          {/* Render all four boxes */}
+          {BentoItems.slice(0, 2).map((item, i) => (
+            <GlassMorphism
+              key={i}
+              variant={'purple-gradient'}
+              className=" md:min-h-[400px] h-full overflow-hidden rounded-xl transition duration-200 shadow-input dark:shadow-none p-4 space-y-4 w-full place-items-center"
+            >
+              <div className="text-sm transition duration-200 w-full flex flex-col items-center">
+                <p className="text-center text-xl font-bold block mb-5">
+                  {item.title}
+                </p>
 
-            <div className="grid place-items-center items-center w-full">
-              {item.content}
-            </div>
-          </GlassMorphism>
-        ))}
+                <p className="text-center text-xs md:text-lg inline-block text-black">
+                  {item.description}
+                </p>
+              </div>
+
+              <div className="grid place-items-center items-center w-full">
+                {item.content}
+              </div>
+            </GlassMorphism>
+          ))}
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 text-black md:gap-10 mt-5 md:mt-10">
+          {BentoItems.slice(2, 4).map((item, i) => (
+            <GlassMorphism
+              key={i}
+              variant={'purple-gradient'}
+              className={` md:min-h-[400px] h-full overflow-hidden rounded-xl transition duration-200 shadow-input dark:shadow-none p-4 md:space-y-4 w-full place-items-center 
+        ${i === 0 ? 'md:col-span-1' : ''}
+        ${i === 1 ? 'md:col-span-2' : ''}`}
+            >
+              <div className="text-sm transition duration-200 w-full flex flex-col text-black items-center">
+                <p className="text-center text-xl font-bold block mb-5">
+                  {item.title}
+                </p>
+                <p className="text-center text-xs md:text-lg inline-block text-black">
+                  {item.description}
+                </p>
+              </div>
+              <div className="grid place-items-center text-black items-center w-full">
+                {item.content}
+              </div>
+            </GlassMorphism>
+          ))}
+        </div>
       </div>
 
 
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-10 mt-5 md:mt-10">
-  {BentoItems.slice(2, 4).map((item, i) => (
-    <GlassMorphism
-      key={i}
-      variant={"light"}
-      className={` md:min-h-[400px] h-full overflow-hidden rounded-xl transition duration-200 shadow-input dark:shadow-none p-4 dark:bg-black dark:-white/[0.2] space-y-4 w-full place-items-center 
-        ${i === 0 ? "md:col-span-1" : ""}
-        ${i === 1 ? "md:col-span-2" : ""}`}
-    >
-      <div className="text-sm transition duration-200 w-full flex flex-col items-center">
-        <p className="text-center text-xl font-bold block mb-5">
-          {item.title}
-        </p>
-        <p className="text-center text-xs md:text-lg inline-block text-[#D2D2D2]">
-          {item.description}
-        </p>
+      {/* dark theme */}
+      <div className="bg-[url('/assets/Barley-Titan.png')] hidden dark:block bg-cover text-white p-10 lg:p-32">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-5 md:gap-10">
+          {/* Render all four boxes */}
+          {BentoItems.slice(0, 2).map((item, i) => (
+            <GlassMorphism
+              key={i}
+              variant={'light'}
+              className=" md:min-h-[400px] h-full overflow-hidden rounded-xl transition duration-200 shadow-input dark:shadow-none p-4 space-y-4 w-full place-items-center"
+            >
+              <div className="text-sm transition duration-200 w-full flex flex-col items-center">
+                <p className="text-center text-xl font-bold block mb-5">
+                  {item.title}
+                </p>
+
+                <p className="text-center text-xs md:text-lg inline-block text-[#D2D2D2]">
+                  {item.description}
+                </p>
+              </div>
+
+              <div className="grid place-items-center items-center w-full">
+                {item.content}
+              </div>
+            </GlassMorphism>
+          ))}
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-5 md:gap-10 mt-5 md:mt-10">
+          {BentoItems.slice(2, 4).map((item, i) => (
+            <GlassMorphism
+              key={i}
+              variant={'light'}
+              className={` md:min-h-[400px] h-full overflow-hidden rounded-xl transition duration-200 shadow-input dark:shadow-none p-4 md:space-y-4 w-full place-items-center 
+        ${i === 0 ? 'md:col-span-1' : ''}
+        ${i === 1 ? 'md:col-span-2' : ''}`}
+            >
+              <div className="text-sm transition duration-200 w-full flex flex-col items-center">
+                <p className="text-center text-xl font-bold block mb-5">
+                  {item.title}
+                </p>
+                <p className="text-center text-xs md:text-lg inline-block text-[#D2D2D2">
+                  {item.description}
+                </p>
+              </div>
+              <div className="grid place-items-center items-center w-full">
+                {item.content}
+              </div>
+            </GlassMorphism>
+          ))}
+        </div>
       </div>
-      <div className="grid place-items-center items-center w-full">
-        {item.content}
-      </div>
-    </GlassMorphism>
-  ))}
-</div>
+
 
     </div>
   );
